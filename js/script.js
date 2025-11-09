9b7e1ffc0b7d6d13b313dc5a640a3d37b94628b0
JosinaldoRodrigues <josinaldor869@gmail.com>
2025-11-09 02:54:49 -0300

    Upd carrinho vendas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
+var current = null;
+document.querySelector('#usuario').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#senha').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#bnt-entrar').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+
 // Toggle sidebar on mobile
 document.getElementById('sidebar-toggle').addEventListener('click', function() {
     document.querySelector('.sidebar').classList.toggle('show');
