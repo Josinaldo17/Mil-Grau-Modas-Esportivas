ae9c5b9fbc2a70c053ed5973c88a8353ab624880
JosinaldoRodrigues <josinaldor869@gmail.com>
2025-12-12 00:48:49 -0300

    Add vite

---
+++ src/components/main.js
@@ -0,0 +1,108 @@
+import '../assets/css/style.css'
+import '../assets/css/vendas.css'
+import logo from '../assets/img/logo.png'
+
+
+document.querySelector('#sidebar').innerHTML = `
+    <div class="sidebar-brand h-25">
+        <img src=${logo}  style="width: 170px; margin: auto;"  >
+        
+    </div>
+    
+    <div class="sidebar-nav flex-grow-1">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-home"></i> 
+                    <span>Inicio</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fa-solid fa-shirt"></i>
+                    <span>Produtos</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-users"></i>
+                    <span>Funcionarios</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span>Vendas</span>
+                </a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-chart-bar"></i>
+                    <span>Relatório</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-cog"></i>
+                    <span>Configuraçoes</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    
+    <div class="p-3">
+        <a href="#" class="btn btn-light d-block text-center">
+            <i class="fas fa-sign-out-alt me-2"></i> Sair
+        </a>
+    </div>
+
+`
+
+document.querySelector('#sidebar_navbar').innerHTML = `
+    <div class="container nav-mobile d-none">
+        <img src="../img/logo.png" class="img-logo-nav navbar-brand" href="#" width="100px">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>   
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="ms-auto"></div>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Funcionarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Vendas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Relatório</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Configuraçoes</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="user-menu">
+        
+        <div class="dropdown">
+            <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                <img src="https://ui-avatars.com/api/?name=John+Doe&background=4361ee&color=fff" alt="User" class="user-avatar">
+                <span class="d-none d-md-inline ms-2">John Doe</span>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+                <li><a class="dropdown-item" href="#"><i class="fas fa-cog me-2"></i> Settings</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt me-2"></i> Sair</a></li>
+            </ul>
+        </div>
+    </div>
+    
+`
